c509dba7d3b69ba2261653763c44bbfd49b7e392
OleksandrDoskaliuk <sashadoskaliyk@gmail.com>
2021-01-19 18:21:31 +0200

    Implemented TaskController for getting all tasks by todo id

--- src/main/java/com/softserve/itacademy/controller/TaskController.java
+++ src/main/java/com/softserve/itacademy/controller/TaskController.java
@@ -1,14 +1,33 @@
 package com.softserve.itacademy.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.softserve.itacademy.service.TaskService;
+import com.softserve.itacademy.service.ToDoService;
 
 @Controller
 @RequestMapping("/tasks")
 public class TaskController {
-
-    //add needed fields
-
+	
+	@Autowired
+	TaskService taskService;
+	
+	@Autowired
+	ToDoService toDoService;
+	
+	@GetMapping("all/todos/{todo_id}")
+	public String getAllTasksByTodoId(@PathVariable(name="todo_id")long todoId,
+			Model model) {
+		model.addAttribute("tasks", taskService.getByTodoId(todoId));
+		model.addAttribute("title", "All Tasks From " + toDoService.readById(todoId).getTitle());
+		return "todo-tasks";
+	}
 //    @GetMapping("/create/todos/{todo_id}")
 //    public String create(//add needed parameters) {
 //        //ToDo
