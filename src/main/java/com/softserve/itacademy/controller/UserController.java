aca4e92165afefa141871ee4cdb73cd42eb2d37f
OleksandrDoskaliuk <sashadoskaliyk@gmail.com>
2021-01-19 20:55:37 +0200

    Implemented method deleteUser(...)

--- src/main/java/com/softserve/itacademy/controller/UserController.java
+++ src/main/java/com/softserve/itacademy/controller/UserController.java
@@ -1,14 +1,25 @@
 package com.softserve.itacademy.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.softserve.itacademy.service.UserService;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
-    //add needed fields
-
+	@Autowired
+	UserService userService;
+	
+	@GetMapping("/delete/{user_id}")
+	public String delete(@PathVariable(name = "user_id")long userId) {
+		userService.delete(userId);
+		return "redirect:/home";
+		}
 //    @GetMapping("/create")
 //    public String create(//add needed parameters) {
 //        //ToDo
@@ -34,11 +45,7 @@ public class UserController {
 //    }
 //
 //
-//    @GetMapping("/{id}/delete")
-//    public String delete(//add needed parameters) {
-//        //ToDo
-//        return " ";
-//    }
+//    
 //
 //    @GetMapping("/all")
 //    public String getAll(//add needed parameters) {
