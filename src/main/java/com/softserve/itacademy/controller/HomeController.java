b616fb7f251af02ba69b63ea5d81109af5c7304c
OleksandrDoskaliuk <sashadoskaliyk@gmail.com>
2021-01-19 12:39:26 +0200

    Implemented method home(...) to return 'users' attribute

--- src/main/java/com/softserve/itacademy/controller/HomeController.java
+++ src/main/java/com/softserve/itacademy/controller/HomeController.java
@@ -1,14 +1,22 @@
 package com.softserve.itacademy.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.softserve.itacademy.service.UserService;
 
 @Controller
 public class HomeController {
-    //ToDo
+	
+	@Autowired
+    UserService userService;
 
-//    @GetMapping({"/", "home"})
-//    public String home(//add needed parameters) {
-//        //ToDo
-//        return "";
-//    }
+    @GetMapping({"/", "home"})
+    public String home(Model model) {
+    	model.addAttribute("users", userService.getAll());
+        return "home";
+    }
 }
