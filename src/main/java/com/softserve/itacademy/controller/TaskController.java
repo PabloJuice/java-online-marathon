0e24e8f5363d6ca7a0aabb60cf2ed976873cb4b0
PabloJuice <pawlo.dzhus@gmail.com>
2021-01-20 16:17:57 +0200

    todo pages

--- src/main/java/com/softserve/itacademy/controller/TaskController.java
+++ src/main/java/com/softserve/itacademy/controller/TaskController.java
@@ -1,7 +1,11 @@
 package com.softserve.itacademy.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.softserve.itacademy.model.User;
+import com.softserve.itacademy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,18 +18,26 @@ import com.softserve.itacademy.service.ToDoService;
 @Controller
 @RequestMapping("/tasks")
 public class TaskController {
-	
+
 	@Autowired
 	TaskService taskService;
-	
+
 	@Autowired
 	ToDoService toDoService;
-	
+
+	@Autowired
+	UserService userService;
+
 	@GetMapping("all/todos/{todo_id}")
 	public String getAllTasksByTodoId(@PathVariable(name="todo_id")long todoId,
 			Model model) {
 		model.addAttribute("tasks", taskService.getByTodoId(todoId));
 		model.addAttribute("title", "All Tasks From " + toDoService.readById(todoId).getTitle());
+		List<User> possible_collaborators = new ArrayList<>(userService.getAll());
+		possible_collaborators.removeAll(toDoService.readById(todoId).getCollaborators());
+		possible_collaborators.remove(toDoService.readById(todoId).getOwner());
+		model.addAttribute("possible_collaborators", possible_collaborators);
+		model.addAttribute("already_collaborators", toDoService.readById(todoId).getCollaborators());
 		return "todo-tasks";
 	}
 //    @GetMapping("/create/todos/{todo_id}")
